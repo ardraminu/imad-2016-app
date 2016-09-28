1fc53ab6db00fbda2535a8baa6d68257d421c630
ardraminu <ardraminu@users.noreply.github.com>
2016-09-28 17:26:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone  = {
+    title:'Article one ardra',
+    heading: 'Article-One',
+    date:' sep 26 2016',
+    content:  
+         <p>
+                  this is the content of my first article.this is the content of my first article. this is the content of my first article.this is the content of my first article.this is the content of my first article .
+         </p>
+         
+          <p>
+                  this is the content of my first article.this is the content of my first article.this is the content of my first article .this is the content of my first article      this is the content of my first article   
+         </p> 
+        
+             <p>
+                   this is the content of my first article. this is the content of my first article . this is the content of my first article.this is the content of my first article. this is the content of my first article.
+             </p>'
+}; 
+
+var html template='<html>
+ <head> 
+   <title>
+        S{title} 
+   </title>
+    <link href="/ui/style.css" rel="stylesheet" />
+  
+   <meta name="viexport" content="width-device-width,initial-scole-1/">
+ </head>
+  <body>
+      <div class="container">
+      <div>
+          <a href="/">home</a>
+      </div>
+          <hr/>
+         <h3>
+            S{heading]
+         </h3>
+         <div> 
+           S{date}
+        
+              </div>
+           
+        <div>
+           S{content}  
+             </div>
+             </div>
+  </body>  
+    
+</html>
+
+
+'; 
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
