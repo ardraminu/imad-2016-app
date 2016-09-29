ee871adedaaf11554b97c866a13548ce54f21d50
ardraminu <ardraminu@users.noreply.github.com>
2016-09-30 00:05:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,10 +86,10 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){ 
-    //articleName == article-one
-    //articles[articleName] == {} content object for article one
+    //articleName = article-one
+    //articles[articleName] = {} content object for article one
     var articleName = req.params.articleName;
-res.send(createTemplate(articles[articlName])); 
+res.send(createTemplate(articles[articleName])); 
 });
 
 
