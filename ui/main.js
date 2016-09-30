1cb44647e60ef60858f425d05ae52777c786a649
ardraminu <ardraminu@users.noreply.github.com>
2016-09-30 21:07:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,17 @@ console.log('Loaded!');
 
 //change the text of the main-text  div
 var element = document.getElementById('main-text');
+
 element.innerHTML = "new value";
 
 //move the image
 var img = document.getElementById('madi');
-img.onclick = function () {
-    img.style.marginleft = "200px";
+marginleft = 0;
+function moveright(){
+     marginleft = marginleft +10;
+     img.style.marginleft = marginleft +"px";
     
-};
+}
+img.onclick = function () {
+      var interval = setInterval(moveright,100);
+ };
