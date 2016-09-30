aa231e8dc6173be69f54cca52323a6100968ee06
ardraminu <ardraminu@users.noreply.github.com>
2016-09-30 23:30:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,10 @@ element.innerHTML = "new value";
 var img = document.getElementById('madi');
 marginLeft = 0;
 function moveRight(){
-     marginLeft = marginLeft + 5;
+     marginLeft = marginLeft + 4;
      img.style.marginLeft = marginLeft + "px";
     
 }
   img.onclick = function () {
-      var interval = setInterval(moveRight, 50);
+      var interval = setInterval(moveRight, 40);
  };
