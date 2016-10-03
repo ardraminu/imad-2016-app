0d21856ab9099cb157b1c9b71c92f8c4e116ffd4
ardraminu <ardraminu@users.noreply.github.com>
2016-10-03 14:23:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ button.onclick = function () {
     var names = ['name1','name2','name3','name4'];
     var list = '';
     for(var i=0; i< names.length; i++){
-        list += '<li>' + names[i] + '</li>';
+        list += names[i];
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
