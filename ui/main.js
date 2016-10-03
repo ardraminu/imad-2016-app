2f0f5146b5a873c0f08f4ea8ce600dceecc09353
ardraminu <ardraminu@users.noreply.github.com>
2016-10-03 14:21:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,12 +33,12 @@ button.onclick = function () {
      // make arequest to the server and send the name
      
      //capture a list of name and render it as a list
-    var names =["name1","name2","name3","name4"];
+    var names = ['name1','name2','name3','name4'];
     var list = '';
-    for(var i=0;i<names.length;i++){
+    for(var i=0; i< names.length; i++){
         list += '<li>' + names[i] + '</li>';
     }
-    var ul = document.getElementById("namelist");
+    var ul = document.getElementById('namelist');
     ul.innerHTML = list;
     
  };
