44cc5b573fab5ea7ba4a404b0eb3583714a94d50
ardraminu <ardraminu@users.noreply.github.com>
2016-10-03 12:59:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,20 @@ button.onclick = function () {
   request.open('GET','http://ardraminu.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+//submit name
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;
+ var sumit = document.getElementById('submit-btn');
+ submit.onclick = function(){
+     // make arequest to the server and send the name
+     
+     //capture a list of name and render it as a list
+    var name =['name1','name2','name3'];
+    var list = '';
+    for(var i=0;i<nmae.length;i++){
+        list +='<li>'+ name[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+ };
