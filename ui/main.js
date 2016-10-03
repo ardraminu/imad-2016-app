e33f3389af74d0d8c3b567be871e2ce701ccd9f3
ardraminu <ardraminu@users.noreply.github.com>
2016-10-03 14:16:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,19 +26,19 @@ button.onclick = function () {
   request.send(null);
 };
 //submit name
- var nameInput = document.getElementById('name');
- var name = nameInput.value;
- var sumit = document.getElementById('submit-btn');
+ var nameInput = document.getElementById("name");
+ var names = nameInput.value;
+ var sumit = document.getElementById("submit-btn");
  submit.onclick = function(){
      // make arequest to the server and send the name
      
      //capture a list of name and render it as a list
-    var names =['name1','name2','name3','name4'];
+    var names =["name1","name2","name3","name4"];
     var list = '';
     for(var i=0;i<names.length;i++){
         list += '<li>' + names[i] + '</li>';
     }
-    var ul = document.getElementById('namelist');
+    var ul = document.getElementById("namelist");
     ul.innerHTML = list;
     
  };
