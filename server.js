a0ab9154dc7d3a4eea729f7fb3d3d479b8768640
ardraminu <ardraminu@users.noreply.github.com>
2016-10-05 17:00:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/counter',function (req, res){
 });
 var names = [];
 app.get('/submit-name',function(req, res){ 
-    var name =req.Query.name;
+    var name =req.query.name;
     
     names.push(name);
     //JSON:Javascript Object Notation
