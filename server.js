d9ecc2888f6cd3afcecbe91b7919f2b959b87e69
ardraminu <ardraminu@users.noreply.github.com>
2016-10-30 14:14:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+var config={
+  user: 'ardraminu',
+  database: 'ardraminu',
+  host: ' db.imad.hasura-app.io',
+  port:'5432',
+  password:process.env. DB_PASSWORD
+  
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -84,11 +93,21 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/test-db', function (req , res){
+var pool = new Pool(config);
+app.get('/article-db', function (req , res){
     //make a select request.
     //return a response with the results
-    
+   pool.query('select * from article',function (err, result){
+       if(err) {
+           res.status(500).send(err.toString());
+           
+       }
+           else {
+               res.send(JSON.stingify(result));
+           }
+       
+       
+   }); 
 });
 var counter = 0;
 app.get('/counter', function (req, res){
